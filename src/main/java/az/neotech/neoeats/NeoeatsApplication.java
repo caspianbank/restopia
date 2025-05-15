f1e7528ad01d981dd95ad2489e8b39e301d10756
Mammad Yahyayev <mammadyahyayev@gmail.com>
2025-05-15 13:30:53 +0400

    initial commit

---
+++ src/main/java/az/neotech/neoeats/NeoeatsApplication.java
@@ -0,0 +1,13 @@
+package az.neotech.neoeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoeatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoeatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/neotech/neoeats/NeoeatsApplicationTests.java
@@ -0,0 +1,13 @@
+package az.neotech.neoeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoeatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
