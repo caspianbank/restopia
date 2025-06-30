421d53b644c6e987a385205dc0938df0c1f2dbf4
Mammad Yahyayev <mammadyahyayev@gmail.com>
2025-06-30 15:48:30 +0400

    add a new method to generate random code with a prefix

--- src/main/java/az/neotech/neoeats/commons/component/RandomCodeGenerator.java
+++ src/main/java/az/neotech/neoeats/commons/component/RandomCodeGenerator.java
@@ -5,13 +5,18 @@ import java.util.Random;
 public final class RandomCodeGenerator {
     private static final String ALPHANUMERIC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-    public static String generateAlphaNumericRandomCode(int length) {
+    public static String generateAlpNumRandomCode(int length) {
         StringBuilder result = new StringBuilder();
         var random = new Random();
         for (int i = 0; i < length; i++) {
             result.append(ALPHANUMERIC.charAt(random.nextInt(ALPHANUMERIC.length())));
         }
         return result.toString();
+    }
+
+    public static String generateAlpNumRandomCode(String prefix, int length) {
+        String randomCode = generateAlpNumRandomCode(length);
+        return prefix + "_" + randomCode;
     }
 
     private RandomCodeGenerator() {
